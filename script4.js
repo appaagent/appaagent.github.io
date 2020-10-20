c31d40ba435d935f5df72ac84d0837402ad48730
appaagent <73074438+appaagent@users.noreply.github.com>
2020-10-21 00:54:58 +0200

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,22 @@
+function onSubmit() {
+  var x= document.getElementById("container");
+	/* Use this line of code to go directly to another page when the correct code is entered and the "submit" button is clicked. */
+          /*if (document.getElementById('password').value == '1234') {window.location.href = 'http:google.com'; }else{ alert('Access Denied, Please try again');}*/
+    
+  /* Use this line of code to show an "Access Granted" linkable button the user must click after entering the correct code and clicking the "submit" button. */
+  /* (the new button that appears must also be clicked to go to the new page.) */ 
+         if (document.getElementById('password').value == '4ppAf33db4ck') { x.style.display = "none";}else{ alert('Access Denied, Please try again');}
+     }
+     
+
+     
+     
+     
+// In the code above, the ".value" sets the passcode. 
+// If you change it to a longer number, make sure you change 
+// the "maxlength" on the index.htm page to match. 
+
+// You can also change the web page in the "href" to another page. 
+// Simply replace "http://google.com" with your desired page. 
+
+// You can also change the "alert" text above to what you want to say.
